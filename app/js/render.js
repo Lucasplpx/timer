227208245537dc004b024c7cfb0bdc7a3d7d3b5d
Lucas Passos <lucasplpx@gmail.com>
2021-08-07 21:46:09 -0300

    02 processos e janelas

---
+++ app/js/render.js
@@ -0,0 +1,7 @@
+const { ipcRenderer } = require('electron');
+
+const linkSobre = document.querySelector('#link-sobre');
+
+linkSobre.addEventListener('click', () => {
+  ipcRenderer.send('abrir-janela-sobre');
+});
---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+app.on('ready', () => {
+  console.log('App started');
+  const mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+let sobreWindow = null;
+ipcMain.on('abrir-janela-sobre', () => {
+  if (sobreWindow == null) {
+    sobreWindow = new BrowserWindow({
+      width: 300,
+      height: 200,
+    });
+
+    sobreWindow.on('closed', () => {
+      sobreWindow = null;
+    });
+  }
+  sobreWindow.loadURL(`file://${__dirname}/app/sobre.html`);
+});
