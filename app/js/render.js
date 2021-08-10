e2da44401cb18de1eebbf532afe02dedabf11a8a
Lucas Passos <lucasplpx@gmail.com>
2021-08-10 19:43:43 -0300

    estrutura inicial

--- app/js/render.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-const linkSobre = document.querySelector('#link-sobre');
-
-linkSobre.addEventListener('click', () => {
-  ipcRenderer.send('abrir-janela-sobre');
-});
