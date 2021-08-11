33155cf5045922dada7481d23c9ba5526ed9d76a
Lucas Passos <lucasplpx@gmail.com>
2021-08-11 19:57:42 -0300

    escopo this e timer

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,14 +1,24 @@
 const { ipcRenderer } = require('electron');
+const timer = require('./timer');
 
 let linkSobre = document.querySelector('#link-sobre');
 const botaoPlay = document.querySelector('.botao-play');
+const tempo = document.querySelector('.tempo');
 
 linkSobre.addEventListener('click', function () {
   ipcRenderer.send('abrir-janela-sobre');
 });
 
 let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+let play = false;
 botaoPlay.addEventListener('click', () => {
   imgs = imgs.reverse();
+  if (play) {
+    timer.parar();
+    play = false;
+  } else {
+    timer.iniciar(tempo);
+    play = true;
+  }
   botaoPlay.src = imgs[0];
 });
