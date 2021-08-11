00e85c8794699de514a592239832c373b06cf35a
Lucas Passos <lucasplpx@gmail.com>
2021-08-11 20:37:41 -0300

    salvando timer e lendo

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,10 +1,18 @@
 const { ipcRenderer } = require('electron');
 const timer = require('./timer');
+const data = require('../../data');
 
 let linkSobre = document.querySelector('#link-sobre');
 const botaoPlay = document.querySelector('.botao-play');
 const tempo = document.querySelector('.tempo');
 const curso = document.querySelector('.curso').innerText;
+
+window.onload = () => {
+  data.pegaDados(curso).then((dados) => {
+    console.log(dados);
+    tempo.textContent = dados.tempo;
+  });
+};
 
 linkSobre.addEventListener('click', function () {
   ipcRenderer.send('abrir-janela-sobre');
--- data.js
+++ data.js
@@ -3,11 +3,26 @@ const { existsSync } = require('fs');
 
 module.exports = {
   salvaDados(curso, tempoEstudado) {
-    const arquivoDoCurso = `${__dirname}/data/${curso}.json`;
-    if (existsSync(arquivoDoCurso)) {
+    const caminhoDoCurso = `${__dirname}/data/${curso}.json`;
+    if (existsSync(caminhoDoCurso)) {
+      this.adicionaTempoAoCurso(caminhoDoCurso, tempoEstudado);
     } else {
-      this.criaArquivoDeCurso(arquivoDoCurso, {}).then(() => {});
+      this.criaArquivoDeCurso(caminhoDoCurso, {}).then(() => {
+        this.adicionaTempoAoCurso(caminhoDoCurso, tempoEstudado);
+      });
     }
+  },
+  adicionaTempoAoCurso(caminhoDoCurso, tempoEstudo) {
+    let dados = {
+      ultimoEstudo: new Date().toString(),
+      tempo: tempoEstudo,
+    };
+    jsonfile
+      .writeFile(caminhoDoCurso, dados, { spaces: 2 })
+      .then(() => {
+        console.log('Tempo salvo com sucesso');
+      })
+      .catch(console.log);
   },
   criaArquivoDeCurso(nomeArquivo, conteudoArquivo) {
     return jsonfile
@@ -18,5 +33,9 @@ module.exports = {
       .catch((err) => {
         console.log(err);
       });
+  },
+  pegaDados(curso) {
+    const caminhoDoCurso = `${__dirname}/data/${curso}.json`;
+    return jsonfile.readFile(caminhoDoCurso);
   },
 };
